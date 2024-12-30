e5d39357b107b18eddf360f50745335b62087180
Dimos Tzanou <d.tzanou91@gmail.com>
2024-12-31 01:46:24 +0200

    project created

--- src/gr/aueb/cf/ch2/SubCalculator.java
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -1,6 +1,15 @@
 package gr.aueb.cf.ch2;
 
 public class SubCalculator {
+
+    public static void subtract() {
+        int num1 = 10;
+        int num2 = 5;
+        int result = num1 - num2;
+
+        System.out.println("The result is: " + result);
+    }
+
     public static int subtract(int a, int b) {
         return a - b;
     }
