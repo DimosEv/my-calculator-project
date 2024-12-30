0178650e4883888904433d7c619fe71952a10125
Dimos Tzanou <d.tzanou91@gmail.com>
2024-12-31 01:31:24 +0200

    New commit message

---
+++ src/gr/aueb/cf/ch1/AddCalculator.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch1;
+
+public class AddCalculator {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/gr/aueb/cf/ch2/SubCalculator.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch2;
+
+public class SubCalculator {
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+}
